f29048c6f739a906693cd9478b78ce6e906cafa8
ikergonzalezgonzalez <iker.gonzalez@opendeusto.es>
2024-06-20 19:33:02 +0200

    crear clase service locator

--- TiendaOnlineServer/src/main/java/psc11/tiendaOnline/Dao/PedidoRepository.java
+++ TiendaOnlineServer/src/main/java/psc11/tiendaOnline/Dao/PedidoRepository.java
@@ -4,7 +4,7 @@ package psc11.tiendaOnline.Dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import psc11.tiendaOnline.DataDomain.Venta;
+
 import psc11.tiendaOnline.DataDomain.Pedido;
 import psc11.tiendaOnline.DataDomain.Usuario;
 

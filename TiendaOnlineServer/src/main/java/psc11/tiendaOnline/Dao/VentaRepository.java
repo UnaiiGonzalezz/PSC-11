cd516eae4ddeb07ecb591dce76325233a0a64747
ikergonzalezgonzalez <iker.gonzalez@opendeusto.es>
2024-06-26 22:47:53 +0200

    server arreglado

--- TiendaOnlineServer/src/main/java/psc11/tiendaOnline/Dao/VentaRepository.java
+++ TiendaOnlineServer/src/main/java/psc11/tiendaOnline/Dao/VentaRepository.java
@@ -3,14 +3,17 @@ package psc11.tiendaOnline.Dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import psc11.tiendaOnline.DataDomain.Plato;
+import psc11.tiendaOnline.DataDomain.Estado;
+import psc11.tiendaOnline.DataDomain.Pedido;
 import psc11.tiendaOnline.DataDomain.Usuario;
 import psc11.tiendaOnline.DataDomain.Venta;
 
-import java.util.List;
+
 
 @Repository
 public interface VentaRepository extends JpaRepository<Venta, Integer> {
-    List<Venta> findByUsuario(Usuario usuario);
-    List<Venta> findByPlato(Plato plato);
+    Venta findById(int id);
+    Venta findByCliente(Usuario cliente);
+    Venta findByPedido(Pedido pedido);
+    Venta findByEstado(Estado estado);
 }
